f725f83d8480c3b66af9740adb3255b31ace0845
SagravJerry <SagravJerry>
2022-05-31 21:30:45 +0800

    Added and modified files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ActionSheetController, AlertController } from '@ionic/angular';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActionSheetController, AlertController, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -8,73 +9,57 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
 })
 export class Tab3Page implements OnInit {
   public tab3: string;
+  public formData: FormGroup;
   public items = [
-    {
-      id: 1,
-      name: 'Pangzkie',
-      number: '09293128545'
-    },
-    {
-      id: 2,
-      name: 'Yotzkie',
-      number: '09293128545'
-    },
-     {
-      id: 3,
-      name: 'Dongzkie',
-      number: '09293128545'
-    },
-    {
-      id: 4,
-      name: 'Bokskie',
-      number: '09293128545'
-    },
-    {
-      id: 5,
-      name: 'banal',
-      number: '09293128545'
-    },
-    {
-      id: 6,
-      name: 'Da Ika',
-      number: '09293128545'
-    },
-     {
-      id: 7,
-      name: 'Tito Bong',
-      number: '09293128545'
-    },
-    {
-      id: 8,
-      name: 'Magdaalena',
-      number: '09293128545'
-    }
-  ]
-
+   
+    
+   
+  ];
   constructor(private activatedRoute: ActivatedRoute, 
-              private alertCtrl:AlertController) {}
+              private alertCtrl:AlertController,
+              private router: Router,
+              private navCtrl: NavController) {}
+
 
   ngOnInit() {
     this.tab3 = this.activatedRoute.snapshot.paramMap.get('id');
-  }
+    this.formData= new FormGroup({
+      name: new FormControl(),
+      lastName: new FormControl(),
+      Phone: new FormControl()
+    }); 
+    
   
-  async alertThis(index: number){
-    await this.alertCtrl.create({
-      header: "Alert",
-      message: " Are you sure?",
-      buttons: [{
-        text: 'yes',
-        role: 'destructive',
-        handler: () => {   
-                this.items.splice(index, 1);  
-          console.log ('delete Clicked');
-        }
-      },
-      {
-        text: 'cancel'
-      }
-      ]
-
-    }).then (res=> res.present());
   }
-}
+  onSubmit() {
+    this.items.push(this.formData.value);
+    //  console.log(this.formData.value);
+    this.formData.reset();
+    
+      };
+
+  
+    async alertThis(index: number): Promise<void>{
+      await this.alertCtrl.create({
+        header: "Alert",
+        message: " Are you sure?",
+        buttons: [{
+          text: 'yes',
+          role: 'destructive',
+          handler: () => {   
+                  this.items.splice(index, 1,);  
+            console.log ('delete Clicked');
+          }
+        },
+        {
+          text: 'cancel'
+        }
+        ]
+  
+      }).then (res=> res.present());
+    }
+    navigateToMessage() {
+      this.navCtrl.navigateForward(['message']);
+    }
+
+}  
